1456ba882ba26dcd40f471968838c2bb99080315
Nesandu Sithnuka <157829526+NesanduGMS@users.noreply.github.com>
2025-02-08 17:53:27 +0530

    Application Process Done

---
+++ client/src/pages/Applicant/AplicationProcess.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Process from '../../components/Applicant/Process'
+
+function AplicationProcess() {
+  return (
+    <div>
+          <Process/>
+    </div>
+  )
+}
+
+export default AplicationProcess
