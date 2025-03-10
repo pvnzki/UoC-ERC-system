5d1e0e89667dd313ff3097a69da3a5d4a18d8ff9
iransamarasekara <161796326+iransamarasekara@users.noreply.github.com>
2025-03-10 13:38:48 +0530

    style: improve code formatting and consistency in Features and Home components

--- client/src/pages/Applicant/Home.jsx
+++ client/src/pages/Applicant/Home.jsx
@@ -1,15 +1,15 @@
-import Hero from '../../components/Applicant/Hero.jsx'
-import Features from '../../components/Applicant/Features.jsx'
-import AboutProcess from '../../components/Applicant/AboutProcess.jsx'
+import Hero from "../../components/Applicant/Hero.jsx";
+import Features from "../../components/Applicant/Features.jsx";
+import AboutProcess from "../../components/Applicant/AboutProcess.jsx";
 
 const Home = () => {
   return (
-    <div>
+    <div className="overflow-hidden">
       <Hero />
       <Features />
       <AboutProcess />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
